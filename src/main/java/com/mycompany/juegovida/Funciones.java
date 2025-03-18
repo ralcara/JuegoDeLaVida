f887ce7e19c51743e4725f9c72452e2276b2199e
ralcara908 <ralcara908@g.educaand.es>
2025-03-18 09:44:11 +0100

    commit 1

---
+++ src/main/java/com/mycompany/juegovida/Funciones.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.juegovida;
+
+/**
+ *
+ * @author rocio
+ */
+public class Funciones {
+     
+    public static void inicializarTablero(){
+        
+    }
+}
