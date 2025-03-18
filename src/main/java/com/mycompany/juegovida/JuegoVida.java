5c0fb45187df18852e0a5d02758d70d1778b7c3a
ralcara908 <ralcara908@g.educaand.es>
2025-03-18 09:54:18 +0100

    pedir datos e inicialización

--- src/main/java/com/mycompany/juegovida/JuegoVida.java
+++ src/main/java/com/mycompany/juegovida/JuegoVida.java
@@ -1,8 +1,10 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.mycompany.juegovida;
+
+import java.util.Random;
+import java.util.Scanner;
 
 /**
  *
@@ -10,7 +12,26 @@ package com.mycompany.juegovida;
  */
 public class JuegoVida {
 
+    public static final int tamañoMaximo = 25;
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner sc = new Scanner(System.in);
+        Random rd = new Random();
+        double porcentaje;
+        int num = 0;
+        int numCelulasVivas;
+        int casillas;
+        int[][] tablero = new int[num][num];
+
+        do {
+            System.out.println("Introduce el tamaño del table");
+            num = sc.nextInt();
+            if (num > 25 || num <= 0) {
+                System.out.println("Vuelve a introducir los datos");
+            }
+        } while (num > 25 || num <= 0);
+        
+        System.out.println("Introduce el porcentaje de celulas vivas");
+        porcentaje = sc.nextDouble();
     }
 }
