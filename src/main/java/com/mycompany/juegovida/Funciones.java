d62545e813927d5e253206847ce145ab704614db
ralcara908 <ralcara908@g.educaand.es>
2025-03-18 10:13:47 +0100

    medio metodo de inicializar tablero

--- src/main/java/com/mycompany/juegovida/Funciones.java
+++ src/main/java/com/mycompany/juegovida/Funciones.java
@@ -4,13 +4,26 @@
  */
 package com.mycompany.juegovida;
 
+import java.util.Random;
+
 /**
  *
  * @author rocio
  */
 public class Funciones {
      
-    public static void inicializarTablero(){
-        
+    public static int[][] inicializarTablero(int num , double porcentaje){
+       int tablero[][] = new int [num][num];
+       int totalCasillas = num*num;
+       int celulasVivas = (int)((porcentaje/100)* totalCasillas);
+       Random rd  = new Random ();
+       
+       do{
+           int columna = rd.nextInt(num);
+           int fila = rd.nextInt(num);
+          
+           
+       }while (celulasVivas>0);
+       return null;
     }
 }
